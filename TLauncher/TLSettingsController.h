14962abbaf0a7fa14b8dc09a6f5081d377a7e026
Daniel Loffgren <daniel@loffgren.org>
2017-04-29 19:04:34 -0700

    Link against CoreServices, which contains LS, instead of LS itself

---
+++ TLauncher/TLSettingsController.h
@@ -0,0 +1,13 @@
+//
+//  TLSettingsController.h
+//  TLauncher
+//
+//  Created by Daniel Loffgren on 4/29/17.
+//  Copyright © 2017 Daniel Loffgren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TLSettingsController : NSArrayController
+
+@end
---
+++ TLauncher/TLSettingsController.m
@@ -0,0 +1,13 @@
+//
+//  TLSettingsController.m
+//  TLauncher
+//
+//  Created by Daniel Loffgren on 4/29/17.
+//  Copyright © 2017 Daniel Loffgren. All rights reserved.
+//
+
+#import "TLSettingsController.h"
+
+@implementation TLSettingsController
+
+@end
